7b942b67722688553d5a20494bf3e25c76c4176a
Shivam-Shane <sk0551460@gmail.com>
2024-05-17 01:38:26 +0530

    project configuration files

---
+++ src/imagecaptioning/entity/__init__.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class ExecuationFlowConfig():
+    data_ingestion_flow: bool
+    data_validation_flow: bool
+    data_transformation_flow: bool
+    model_trainer_flow: bool
+    model_evaluation_flow: bool
+
+@dataclass
+class DataIngestionConfig():
+    data_path: Path
+    data_ingestion_path: Path
+
+@dataclass
+class DataTransformationConfig():
+    data_ingestion_path: Path
+    data_transformation_path: Path
---
+++ src/imagecaptioning/pipeline/data_transformation.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from src.imagecaptioning.config.configuration import ConfigurationManager
+from src.imagecaptioning.components.data_transformation import DataTransformation
+from logger import logging
+
+@dataclass
+class DataTransformationPipeline():
+    def main(self):
+       get_configuratiomanger =ConfigurationManager()
+       get_datatransformationconfig_details=get_configuratiomanger.get_data_transformation_config()
+       datatransformation=DataTransformation(config=get_datatransformationconfig_details)
+       datatransformation.convert_images_to_features()
