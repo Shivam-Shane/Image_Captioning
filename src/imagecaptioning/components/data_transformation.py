90f9ebad9ebf3dd87786e73ff5bdd0c3c7f6558c
Shivam-Shane <sk0551460@gmail.com>
2024-05-21 19:59:40 +0530

    Captions mapping and cleaning

--- src/imagecaptioning/components/data_transformation.py
+++ src/imagecaptioning/components/data_transformation.py
@@ -10,26 +10,33 @@ from src.imagecaptioning.config.configuration import DataTransformationConfig
 from logger import logging
 import warnings
 warnings.filterwarnings("ignore")
+
+#------------------------------------------------------------------------------------------------
 class DataTransformation():
     def __init__(self,config:DataTransformationConfig):
         self.config=config
         
-
     def convert_images_to_features(self):
+        """Converting images to features/Tf-idf for preprocessing
+        Args: 
+            None
+        Returns: 
+                Saves the features into pickle file.
+        """
         logging.info(f">>>> Inside {self.__class__.__name__}.{self.convert_images_to_features.__name__}")
         try:
             self.model=VGG16(weights='imagenet',include_top=True)#fully connected layers responsible for classification are included.
-            self.feature_extract=Model(inputs=self.model.input,outputs=self.model.layers[-2].output)# taking only features from , not the last output layer
-            features={}
+            self.feature_extract=Model(inputs=self.model.input,outputs=self.model.layers[-2].output)# taking only features layers from VGG16 , not the last output(Softmax) layer
+            features={} # Dictonary for stroing the features
             for image_name in tqdm(os.listdir(Path(self.config.data_ingestion_path)),desc='Processing'):
                 img_path = os.path.join(Path(self.config.data_ingestion_path), image_name)
-                image=load_img(img_path,target_size=(224,224)) #converting images to pixel
-                image=img_to_array(image)
-                image=image.reshape(1,image.shape[0],image.shape[1],image.shape[2])#reshapping data for model
-                image=preprocess_input(image)
-                # Extracting the features from the features extracter haiving layers from imagenet predefined dataset
+                image=load_img(img_path,target_size=(224,224))# lOADING THE IMAGE  
+                image=img_to_array(image)#converting images to pixel
+                image=image.reshape(1,image.shape[0],image.shape[1],image.shape[2])#rReshapping data for model
+                image=preprocess_input(image)# processing input image using VGG16 preprocess_input function
+                #Extracting the features from the features extracter haiving layers from imagenet predefined dataset
                 features_extracted=self.feature_extract.predict(image,verbose=0)
-                #stroing the extracted features with image IDs
+                #storing the extracted features with image IDs
                 image_id =image_name.split('.')[0]
                 features[image_id] =features_extracted
             logging.debug(f"Saving features at path {self.config.data_transformation_save_features}")
@@ -43,14 +50,22 @@ class DataTransformation():
             logging.error(e)
             raise e
     def map_captions_to_images(self):
+        """Mapping Captions Id to Images ID
+        Args: 
+            Captions Id and Images to map
+        Returns: 
+                Saves the mapped features into pickle file.
+        """
         logging.info(f">>>> Inside {self.__class__.__name__}.{self.map_captions_to_images.__name__}")
         try:
             mapping={}
-            with open(os.path.join(self.config.data_path,"caption.txt"),"r") as file:
+            with open(os.path.join(self.config.data_path,"captions.txt"),"r") as file:
                 next(file)
                 caption_document=file.read() #storing all the captions
             for line in tqdm(caption_document.split("\n")):
                 token=line.split(",")
+                if len(token)<2:
+                    continue
                 image_id,caption=token[0],token[1]
                 #Removing extextion from image id
                 image_id=image_id.split(".")[0]
@@ -60,17 +75,29 @@ class DataTransformation():
                 if image_id not in mapping:
                     mapping[image_id]=[]
                 mapping[image_id].append(caption)
-            def caption_cleaining(mapping):
+
+            def caption_cleaning(mapping):
+                """Cleans all captions data
+                Args: mapping: dictionary mapped IDs
+                
+                Return:
+                        Cleaned captions data in form of pickle
+                """
                 for key,caption in mapping.items():
                     for i in range(len(caption)):
                         caption=caption[i]
                         caption=caption.lower()
                         caption=caption.replace("[^A-Za-z]","")
                         # caption=caption.replace("\s+","")# additional spaces removal
-                        caption="<start>"+" ".join([word for word in caption.split() if len(word)>=1])+"<end>"
+                        caption="<start>"+" ".join([word for word in caption.split() if len(word)>=1])+"<end>" # added start and end to get captions
             
-            caption_cleaining(mapping)# cleaning the caption list   
-                        
+            cleaned_caption=caption_cleaning(mapping)# cleaning the caption list   
+            logging.debug(f"Saving mapped caption at path {self.config.data_transformation_save_captions}")         
+            with open(os.path.join(self.config.data_transformation_save_captions,"captions.pkl"),"wb")as file:
+                pickle.dump(cleaned_caption,file)
+                logging.info(f"Features saved at path {self.config.data_transformation_save_features} with name captions.pkl")
+
         except Exception as e:
             logging.error(e)
             raise e
+#------------------------------------------------------------------------------------------------
