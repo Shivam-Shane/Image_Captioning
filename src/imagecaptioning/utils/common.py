d730bc34d5dfb01c4de1ae6ca2c200f4c43ebf1c
Shivam-Shane <sk0551460@gmail.com>
2024-05-26 01:13:59 +0530

    update yaml file function added

--- src/imagecaptioning/utils/common.py
+++ src/imagecaptioning/utils/common.py
@@ -32,7 +32,55 @@ def read_yaml(path_to_yaml)-> ConfigBox: #reading yaml
         raise ValueError("yaml file is empty")
     except Exception as e: 
         raise e   
-                         
+def write_yaml(path_to_yaml,updated_data):
+    """
+    Write a ConfigBox to a YAML file.
+
+    Args:
+        path_to_yaml (str): The path to the YAML file.
+        updated_data (ConfigBox): The updated data to write to the YAML file.
+    """
+    logging.info(f"Starting writing yaml file {path_to_yaml}")
+    try: 
+        with open(path_to_yaml, "w") as yaml_file:
+            yaml.dump(updated_data,yaml_file,default_flow_style=False)
+            
+            logging.info(f"yaml file: {path_to_yaml} write done successfully")    
+    except Exception as e:
+        raise e
+
+def update_yaml(path_to_yaml,updates):
+    """
+    Update specified keys in a yaml file with new values
+
+    Args:
+        path_to_yaml (str): The path to the YAML file.
+        updates (dict): The updates to be made to the YAML file in form of dict, key:value
+    
+    Raises:
+        Exception: If an error occurs while updating the YAML file.
+
+    """
+    try:
+        # Read the YAML file 
+        yaml_data = read_yaml(path_to_yaml)
+
+        # Update the yaml file with new values
+        for key, value in updates.items():
+            keys=key.split('.') # Split key for nested updates
+            d=yaml_data
+            for k in keys[:-1]:
+                d=d.setdefault(k,{})# Checks if the key k exists in the current dictionary d,creates a new dictionary if not exists
+            d[keys[-1]]=value
+        yaml_data=yaml_data.to_dict() # saving to dictionary as read_yaml return config Box 
+
+        # write data back to yaml file
+        write_yaml(path_to_yaml,yaml_data)
+        logging.info(f"yaml file: {path_to_yaml} updated successfully with {updates}")
+    except Exception as e:
+        raise e
+
+
 def create_directories(path_to_directory: list[str], verbose=True): # create directories for all files
     """Create directories if they don't exist.
     Args:
