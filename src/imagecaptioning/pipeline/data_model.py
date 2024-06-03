a807b19b6685bf9b9ad0c3ea12c42e77e0fdf0a6
Shivam-Shane <sk0551460@gmail.com>
2024-06-04 00:34:12 +0530

    Updated model training

--- src/imagecaptioning/pipeline/data_model.py
+++ src/imagecaptioning/pipeline/data_model.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from src.imagecaptioning.config.configuration import ConfigurationManager
 from src.imagecaptioning.components.data_model import DataModel
-from logger import logging
 
 @dataclass
 class DataModelPipeline():
@@ -10,3 +9,4 @@ class DataModelPipeline():
         get_datamodelconfig_details=get_configuratiomanger.get_data_model_config()
         datamodel=DataModel(config=get_datamodelconfig_details)
         datamodel.update_model_parameters()
+        datamodel.model_training()
