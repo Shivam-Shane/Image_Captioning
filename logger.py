e3782b9cfaf974cdf08aea5fe635cd70956b07d5
Shivam-Shane <sk0551460@gmail.com>
2024-06-27 18:56:44 +0530

    Enchanced comments

--- logger.py
+++ logger.py
@@ -2,15 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-logger=logging.getLogger("ImageCaptioning")
+logger=logging.getLogger("ImageCaptioning") #using named loggers, we can control the logging level for different parts
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H')}.log" #What type of log name to use
-LOGS_PATH = os.path.join(os.getcwd(), "LOGS", LOG_FILE)       
-os.makedirs(LOGS_PATH, exist_ok=True)
-LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE) # where to save log files
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H')}.log" #What type of log file name to use
+LOGS_PATH = os.path.join(os.getcwd(), "LOGS", LOG_FILE)    # Path where the log will be stored   
+os.makedirs(LOGS_PATH, exist_ok=True)                      # Making sure the directory exists
+LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE) # Saving the log file path for log files
 
 logging.basicConfig(
             filename=LOG_FILE_PATH,
             format="[%(asctime)s %(lineno)s] %(levelname)s %(message)s ",
             level=logging.INFO,
-            )
+            )    # basin config for our logging, we can chanage as per our needs.
+
+#-----Learn At https://docs.python.org/3/howto/logging.html---------
