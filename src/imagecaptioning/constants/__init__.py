e92a309ba04313d40603a10d738fe50442eb0454
Shivam-Shane <sk0551460@gmail.com>
2024-06-27 20:09:01 +0530

    Enhanced comments

--- src/imagecaptioning/constants/__init__.py
+++ src/imagecaptioning/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH=Path("config/config.yaml")
-PARMS_FILE_PATH=Path("PARMS.YAML")
+CONFIG_FILE_PATH=Path("config/config.yaml") # path to config file, which store configuration code for the project
+PARMS_FILE_PATH=Path("PARMS.YAML")          # path to parms.yaml file, stores model hyper-parameters details.
