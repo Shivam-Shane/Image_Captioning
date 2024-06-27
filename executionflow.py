c22b93990218562a3b87b78aaed077d433699d47
Shivam-Shane <sk0551460@gmail.com>
2024-06-27 20:06:38 +0530

    Enhanced comments

--- executionflow.py
+++ executionflow.py
@@ -9,20 +9,24 @@ class Executionflow():
         logging.info(f">>>> Inside {self.__class__.__name__}.{self.__init__.__name__}")
         self.config=read_yaml(config_filepath), # Reading config yaml file for execution flow
     
-    def get_data_for_executionflow(self)->ExecuationFlowConfig: #Returing object of type ExecutionFlowConfig needed to our entity module
+    def get_data_for_executionflow(self)->ExecuationFlowConfig: # Returing object of type ExecutionFlowConfig needed to our entity module
         """Get keys for execution flow .
         Args:
             None
         Returns:
-        ExecutionflowConfig: Having values for corresponding keys related to execution flow
+        ExecutionflowConfig: Having values for corresponding keys related to execution flow.
         """
-        logging.info(f">>>> Inside {self.__class__.__name__}.{self.get_data_for_executionflow.__name__}")
-        config=self.config[0].execution_flow
-        execution_flow_config=ExecuationFlowConfig(
-                                data_ingestion_flow=config.data_ingestion_flow,
-                                data_transformation_flow=config.data_transformation_flow,
-                                model_trainer_flow=config.model_trainer_flow,
-                                model_evaluation_flow=config.model_evaluation_flow
-                                # add more execution flow here, and in entity and config.yaml files
-        )
-        return execution_flow_config
+        try:
+            logging.info(f">>>> Inside {self.__class__.__name__}.{self.get_data_for_executionflow.__name__}")
+            config=self.config[0].execution_flow
+            execution_flow_config=ExecuationFlowConfig(
+                                    data_ingestion_flow=config.data_ingestion_flow,
+                                    data_transformation_flow=config.data_transformation_flow,
+                                    model_trainer_flow=config.model_trainer_flow,
+                                    model_evaluation_flow=config.model_evaluation_flow
+                                    # add more execution flow here, and in entity and config.yaml files
+            )
+            return execution_flow_config
+        except Exception as e:
+            raise e
+        
