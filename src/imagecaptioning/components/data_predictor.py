dbdecf799f7c5f7fa6961e13e9f95aba10d5aa7c
Shivam-Shane <sk0551460@gmail.com>
2024-06-29 02:05:11 +0530

    Updated comments, changed variable to more meaning full words

--- src/imagecaptioning/components/data_predictor.py
+++ src/imagecaptioning/components/data_predictor.py
@@ -23,19 +23,20 @@ class Caption_Predictor:
         self.config = config
         self.parms = read_yaml(PARMS_FILE_PATH)
         
-        if not Caption_Predictor._runonce:
+        if not Caption_Predictor._runonce: # making sure we don't call this twice reducing workload time
             try:
-                logging.info("Loading data model")
+                logging.info(f"Loading saved model keras file from path {self.config.data_model_path}")
                 Caption_Predictor._model = load_model(Path(os.path.join(self.config.data_model_path, "model.keras")))
 
-                logging.info("Loading tokenizer file")
+                logging.info(f"Loading feature tokenizer file from path {self.config.data_model_tokenizer}")
                 with open(Path(os.path.join(self.config.data_model_tokenizer, "tokenizer.json"))) as f:
                     tokenizer_json = f.read()
                 Caption_Predictor._tokenizer = tokenizer_from_json(tokenizer_json)
 
-                # Load the VGG16 model pre-trained on ImageNet
-                self.vgg_model = ResNet50(include_top=True, weights='imagenet')
-                Caption_Predictor._feature_extractor = Model(inputs=self.vgg_model.inputs, outputs=self.vgg_model.layers[-2].output)
+                # Load the Resnet50 model, pre-trained on ImageNet dataste
+                self.renet_model = ResNet50(include_top=True, weights='imagenet')
+                # Extracting features layer from the resnet50 model
+                Caption_Predictor._feature_extractor = Model(inputs=self.renet_model.inputs, outputs=self.renet_model.layers[-2].output)
                 Caption_Predictor._runonce = True
             except Exception as e:
                 raise e
@@ -46,12 +47,20 @@ class Caption_Predictor:
         self.feature_extractor = Caption_Predictor._feature_extractor
 
     def preprocess_image(self, input_image):
+        """ Transform the input image
+        Args:
+            input_image:Path
+        Returns:
+                features_extracted: Transformed input_image features
+        Raises:
+            Exception: if anything goes wrong
+        """
         try:
             logging.info(f"<<<< Start of {self.__class__.__name__}.{self.preprocess_image.__name__}")
             image = load_img(input_image, target_size=(224, 224))  # Loading the image
-            image = img_to_array(image)  # Converting images to pixel
-            image = np.expand_dims(image, axis=0)  # Reshaping data for model
-            image = preprocess_input(image)  # Processing input image using VGG16 preprocess_input function
+            image = img_to_array(image)             # Converting images to pixel
+            image = np.expand_dims(image, axis=0)   # Reshaping data for model
+            image = preprocess_input(image)         # Processing input image using VGG16 preprocess_input function
             # Extracting the features from the features extractor having layers from ImageNet predefined dataset
             features_extracted = self.feature_extractor.predict(image, verbose=0)
             logging.info(f">>>> End of {self.__class__.__name__}.{self.preprocess_image.__name__}")
@@ -59,29 +68,40 @@ class Caption_Predictor:
         except Exception as e:
             raise e
 
-    def generates_captions(self, input_image):
+    def generates_captions(self, feature_image):
+        """Generates Captions for the given feature image
+        Args:
+            FeatureImage: Transformed image features
+        Returns:
+                Caption: Captions for the given feature image
+        Raises:
+            Exception: if anything goes wrong
+        """
         logging.info(f"<<<< Start of {self.__class__.__name__}.{self.generates_captions.__name__}")
 
         in_text = "startcaption"
         try:
             for i in range(self.parms.Model_Arguments.max_length):
+                # Convert the input text to a sequence of integers using the tokenizer
                 sequence = self.tokenizer.texts_to_sequences([in_text])[0]
+                # Padding the sequence to ensure it has the required length
                 sequence = pad_sequences([sequence], maxlen=self.parms.Model_Arguments.max_length)
-                yhat = self.model.predict([input_image, sequence], verbose=0)
-                yhat = np.argmax(yhat)
-                word = self.tokenizer.index_word[yhat]
+                yhat_prob = self.model.predict([feature_image, sequence], verbose=0) # 'verbose=0' suppresses the output of the prediction process
+                yhat_max_prob_index = np.argmax(yhat_prob)  # Index of the highest probability in the predicted output for current sequence
+                word = self.tokenizer.index_word[yhat_max_prob_index] 
+                # Retrieve the word corresponding to the predicted index from the tokenizer's index_word vocabulary
                 if word is None:
                     logging.info('word not found')
                     break
                 in_text += ' ' + word
                 if word == 'endcaption':
-                    logging.debug('endcaption found')
+                    logging.info('endcaption found')
                     break
             logging.info(f">>>> End of {self.__class__.__name__}.{self.generates_captions.__name__}")
             logging.info(in_text)
-            pattern = r'startcaption\s*(.*?)\s*endcaption'
-            cleaned_text = re.sub(pattern, r'\1', in_text, flags=re.DOTALL)
-            return cleaned_text.strip()
+            pattern = r'startcaption\s*(.*?)\s*endcaption'  # pattern for removing unnecessary words[startcaption, endcaption].
+            cleaned_text = re.sub(pattern, r'\1', in_text, flags=re.DOTALL)  
+            return cleaned_text.strip()  # any leading and trailing whitespace removed
         except Exception as e:
             raise e
 # -----------------------------------------------------------------------------------------------------------------------------------------------
