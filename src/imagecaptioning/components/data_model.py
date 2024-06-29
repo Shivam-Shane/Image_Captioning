33508c58bc8e9d87497ee8a1d87150378fc154dc
Shivam-Shane <sk0551460@gmail.com>
2024-06-29 18:27:19 +0530

    Model checkpoint, learning rate schedule callback, continue training from checkpoint if have, updated comments,

--- src/imagecaptioning/components/data_model.py
+++ src/imagecaptioning/components/data_model.py
@@ -7,8 +7,10 @@ from src.imagecaptioning.config.configuration import DataModelConfig
 from tensorflow.keras.preprocessing.text import Tokenizer #type:ignore
 from tensorflow.keras.preprocessing.sequence import pad_sequences #type:ignore
 from tensorflow.keras.utils import to_categorical #type:ignore
-from tensorflow.keras.layers import Input,Dense,LSTM,Embedding,Dropout,add #type:ignore
-from tensorflow.keras.models import Model  #type:ignore
+from tensorflow.keras.layers import Input,Dense,Embedding,Dropout,add,BatchNormalization, GRU #type:ignore
+from tensorflow.keras.optimizers import Adam #type:ignore
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler #type:ignore
+from tensorflow.keras.models import Model,load_model  #type:ignore
 from src.imagecaptioning.constants import *
 from src.imagecaptioning.utils.common import read_yaml,update_yaml
 from logger import logging
@@ -75,23 +77,27 @@ class DataModel():
             Image: all images
         Returns:
             Numpy arrays
+        Raises:
+            Exception: if anything goes wrong
         """
         logging.debug(f">>>> Inside {self.__class__.__name__}.{self.update_model_parameters.__name__}")
+        try:
+            X1, X2, y = list(), list(), list()  # Initialize empty lists to hold image features, input sequences, and output words
+            for caption in captions_list:       # Iterate over each caption
+                seq = self.tokenizer.texts_to_sequences([caption])[0]   # Convert the caption to a sequence of integers
 
-        X1, X2, y = list(), list(), list()  # Initialize empty lists to hold image features, input sequences, and output words
-        for caption in captions_list:       # Iterate over each caption
-            seq = self.tokenizer.texts_to_sequences([caption])[0]   # Convert the caption to a sequence of integers
+                for i in range(1, len(seq)):                    # Create multiple input-output pairs from the caption
+                    in_seq, out_seq = seq[:i], seq[i]           # Split the sequence into input (first i words) and output (i-th word)
+                    in_seq = pad_sequences([in_seq], maxlen=self.parms.Model_Arguments.max_length)[0]           # Pad the input sequence to the maximum length
+                    out_seq = to_categorical([out_seq], num_classes=self.parms.Model_Arguments.vocab_size)[0]   # One-hot encode the output word
+                    X1.append(image)                            # Adding the image feature to X1
+                    X2.append(in_seq)                           # Adding the input sequence to X2
+                    y.append(out_seq)                           # Adding the one-hot encoded output word to y
+            logging.debug(f">>>> End of {self.__class__.__name__}.{self.create_captions_sequence.__name__}")      
+            return np.array(X1), np.array(X2), np.array(y)      # Return the arrays as numpy arrays
+        except Exception as e:
+            raise e
 
-            for i in range(1, len(seq)):                    # Create multiple input-output pairs from the caption
-                in_seq, out_seq = seq[:i], seq[i]           # Split the sequence into input (first i words) and output (i-th word)
-                in_seq = pad_sequences([in_seq], maxlen=self.parms.Model_Arguments.max_length)[0]           # Pad the input sequence to the maximum length
-                out_seq = to_categorical([out_seq], num_classes=self.parms.Model_Arguments.vocab_size)[0]   # One-hot encode the output word
-                X1.append(image)                            # Adding the image feature to X1
-                X2.append(in_seq)                           # Adding the input sequence to X2
-                y.append(out_seq)                           # Adding the one-hot encoded output word to y
-        logging.debug(f">>>> End of {self.__class__.__name__}.{self.create_captions_sequence.__name__}")      
-        return np.array(X1), np.array(X2), np.array(y)      # Return the arrays as numpy arrays
-        
     def data_generator(self,captions, image_features):      # Ensure that not all dataset is loaded at once in memory
         """ Generates Data in batches for model
         Args:
@@ -104,16 +110,16 @@ class DataModel():
         
         while True:
             logging.debug("Generating batch fixes for training")
-            keys=list(captions.keys())                      # List all the images ID
-            np.random.shuffle(keys)                         # Shuffling the keys to ensure randomness
-            batch_size=self.parms.Model_Arguments.batch_size # Getting the batch size
+            keys=list(captions.keys())                        # List all the images ID
+            np.random.shuffle(keys)                           # Shuffling the keys to ensure randomness
+            batch_size=self.parms.Model_Arguments.batch_size  # Getting the batch size
             for i in range(0, len(keys),batch_size):          # Iterate over the keys in the batch_size increament
-                X1, X2, y = list(), list(), list()          # Empty list for current batch
+                X1, X2, y = list(), list(), list()            # Empty list for current batch
                 for j in range(i,min(i+batch_size,len(keys))) : # for each key in the current batch
                     key=keys[j]
-                    image=image_features[key]              # Get the image feature for the current key
+                    image=image_features[key]                   # Get the image feature for the current key
                     if image.shape != (2048,):
-                        logging.critical(f"Expected image shape (4096,), got {image.shape}")
+                        logging.critical(f"Expected image shape (2048,), got {image.shape}")
                     captions_list=captions[key]                # Get the captions for the current key
                     in_img,in_seq,out_word=self.create_captions_sequence(captions_list,image) # Create the sequence
                     X1.extend(in_img)                       # extend the image
@@ -131,22 +137,26 @@ class DataModel():
             modelwrappers
         """
         logging.info(f">>>> Inside {self.__class__.__name__}.{self.model_definition.__name__}")
-        image_inputs1=Input(shape=(2048,)) # Image Layer
-        feature1=Dropout(0.5)(image_inputs1)
-        feature2=Dense(256,activation="relu")(feature1)
-
-        text_inputs2=Input(shape=(self.parms.Model_Arguments.max_length,)) # Caption Layer
-        seq2=Embedding(self.parms.Model_Arguments.vocab_size,256,mask_zero=True)(text_inputs2)
-        seq2=Dropout(0.5)(seq2)
-        seq2=LSTM(256)(seq2)
-
+        image_inputs1 = Input(shape=(2048,))  # Image Layer
+        feature1 = Dropout(0.5)(image_inputs1)
+        feature2 = Dense(512, activation="relu")(feature1)
+        feature2 = BatchNormalization()(feature2)
         
-        decoder1=add([feature2,seq2])  # Adding Images and captions together
-        decoder1=Dense(256,activation="relu")(decoder1)
-        outputs=Dense(self.parms.Model_Arguments.vocab_size,activation="softmax")(decoder1)
-
-        model=Model(inputs=[image_inputs1,text_inputs2],outputs=outputs)
-        model.compile(loss="categorical_crossentropy",optimizer="adam")
+        # Caption processing
+        text_inputs2 = Input(shape=(self.parms.Model_Arguments.max_length,))  # Caption Layer
+        seq2 = Embedding(self.parms.Model_Arguments.vocab_size, 256, mask_zero=True)(text_inputs2)
+        seq2 = Dropout(0.5)(seq2)
+        seq2 = GRU(512, return_sequences=True)(seq2)
+        seq2 = GRU(512)(seq2)
+        
+        # Decoder (combining image features and text features)
+        decoder1 = add([feature2, seq2])
+        decoder1 = Dense(512, activation="relu")(decoder1)
+        decoder1 = BatchNormalization()(decoder1)
+        outputs = Dense(self.parms.Model_Arguments.vocab_size, activation="softmax")(decoder1)
+        
+        model = Model(inputs=[image_inputs1, text_inputs2], outputs=outputs)
+        model.compile(loss="categorical_crossentropy", optimizer=Adam(learning_rate=1e-4))
 
         logging.debug(f"Model Summary{model.summary}")
         logging.info(f">>>> End of {self.__class__.__name__}.{self.model_definition.__name__}")
@@ -158,22 +168,50 @@ class DataModel():
             None
         Returns: 
                 None
+        Raises:
+            Exception: if anything goes wrong
         """
         logging.info(f">>>> Inside {self.__class__.__name__}.{self.model_training.__name__}")
-        logging.info("-----Starting Model Training------")
 
         steps=len(self.captions) // self.parms.Model_Arguments.batch_size
         logging.debug("Calling model definition")
         model=self.model_definition()
+
+        # Define the checkpoint callback
+        checkpoint_callback = ModelCheckpoint(
+            filepath=os.path.join(self.config.data_model_path, 'model_checkpoint.keras'),  # File path where the model weights will be saved
+            monitor='loss',              # Monitor the training loss
+            save_best_only=True,         # Save only the best model
+            save_weights_only=False,     # Save the full model (architecture + optimizer state + weights)
+            verbose=1                    # message when a model checkpoint is saved
+        )
+        def learning_rate_scheduler(epoch, lr):
+            if epoch > 10:
+                return lr * 0.1
+            return lr
+
+        lr_scheduler_callback = LearningRateScheduler(learning_rate_scheduler)  # learning rate scheduler 
+        
+        # Combineing all callbacks
+        callbacks_list = [checkpoint_callback, lr_scheduler_callback]
+
         logging.debug(f"starting training in batches with batch size {self.parms.Model_Arguments.batch_size}")
+        is_checkpoint = "No checkpoint found, starting training from scratch"
+        if os.path.exists(Path(os.path.join(self.config.data_model_path, 'model_checkpoint.keras'))):
+            model = load_model(Path(os.path.join(self.config.data_model_path, 'model_checkpoint.keras')))
+            is_checkpoint="Starting training from checkpoint"
+        logging.info(is_checkpoint)
+
         model.fit(
             self.data_generator( self.captions, self.features),
-            epochs=self.parms.Model_Arguments.epochs,
-            steps_per_epoch=steps,
-            verbose=1)
+            epochs=self.parms.Model_Arguments.epochs,         # Number of epochs to train
+            steps_per_epoch=steps,                            # Number of steps
+            verbose=1,                                        # On terminal output
+            callbacks=callbacks_list                          # Callbacks list [checkpoint, LRS]
+            )
         try:
             logging.info("Saving model pickle to keras file")
-            model.save(os.path.join(self.config.data_model_path,"model.keras"))
+            model.save(os.path.join(self.config.data_model_path,"model.keras"))  # saving the last model in pickle file
         except Exception as e:
                 raise e 
         logging.info(f">>>> End of {self.__class__.__name__}.{self.model_training.__name__}")
