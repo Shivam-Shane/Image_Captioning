fd8e8e908c7fec6f713f2eb3297e3d6dd84e57c5
Shivam-Shane <sk0551460@gmail.com>
2024-07-18 00:01:45 +0530

    fixed case-senstive issue, rmeoved path

--- src/imagecaptioning/constants/__init__.py
+++ src/imagecaptioning/constants/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
+# Define the root directory for the project
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent
-CONFIG_FILE_PATH=ROOT_DIR / Path("config/config.yaml") # path to config file, which store configuration code for the project
-PARMS_FILE_PATH=ROOT_DIR / Path("/PARMS.YAML")          # path to parms.yaml file, stores model hyper-parameters details.
+
+CONFIG_FILE_PATH=ROOT_DIR / "config/config.yaml" # path to config file, which store configuration code for the project
+PARMS_FILE_PATH=ROOT_DIR / "parms.yaml"          # path to parms.yaml file, stores model hyper-parameters details.
